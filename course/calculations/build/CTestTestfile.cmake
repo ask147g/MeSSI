90c56474fe3bb4b791c886d0d29a7337b8731047
veoutlo <veoutlo@gmail.com>
2022-01-31 08:47:30 +0700

    deleted

--- course/calculations/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/ask147/WINDOWS/Users/Anna/Documents/MATLAB/MP/course/calculations
-# Build directory: /media/ask147/WINDOWS/Users/Anna/Documents/MATLAB/MP/course/calculations/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
