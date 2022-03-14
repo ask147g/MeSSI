a0d31f81de45685739ba5a1a04cd2ac7c01ac57c
veoutlo <veoutlo@gmail.com>
2022-03-14 18:59:12 +0700

    ready

--- lab1.m
+++ lab1.m
@@ -12,13 +12,30 @@ L = 0.6;
 pa = 13300;
 c1 = 0.264;
 c2 = 0.269;
+c3 = 0.089/100;
+c4 = 1.91/100;
+c5 = 4.071/100;
+c6 = 21.232/100;
+c7 = 10.436/100;
+c8 = 0.095/100;
+c9 = 8.857/100;
 mu1 = 129*10^(-3);
 mu2 = 132*10^(-3);
+mu3 = 126*10^(-3);
+mu4 = 128*10^(-3);
+mu5 = 130*10^(-3);
+mu6 = 131*10^(-3);
+mu7 = 134*10^(-3);
+mu8 = 124*10^(-3);
+mu9 = 136*10^(-3);
 
 %% Расчет
-mu = mu1*c1 + mu2*c2; % средний вес
+mu = mu1*c1 + mu2*c2 + mu3*c3 + mu4*c4 + mu5*c5 + mu6*c6 + mu7*c7 + mu8*c8 + mu9*c9; % средний вес
+disp('mu='); disp(mu);
 D = 33.25*10^(-2)/pa * sqrt(T/mu); % коэффициент диффузии
+disp('D='); disp(D);
 p_mix = pa.*[c1, c2];
+disp('p_mix='); disp(p_mix);
 r = 0.03:0.01:0.06;
 p = zeros(2,length(r));
 for i=1:length(r)
@@ -53,3 +70,9 @@ subplot(1,2,2);
 plot(rr(length(rr)-4:length(rr)),pr(1,length(rr)-4:length(rr)),'ks-',rr(length(rr)-4:length(rr)),pr(2,length(rr)-4:length(rr)),'r.-'); grid on;
 xlabel('r_a, м', 'FontSize', 14, 'FontName', 'TimesNewRoman');
 ylabel('p, Па', 'FontSize', 14, 'FontName', 'TimesNewRoman');
+figure(4);
+plot(rr(57:59),pr(1,(57:59)),'ks-',rr(57:59),pr(2,57:59),'r.-'); grid on;
+xlabel('r_a, м', 'FontSize', 14, 'FontName', 'TimesNewRoman');
+ylabel('p, Па', 'FontSize', 14, 'FontName', 'TimesNewRoman');
+title('Точка пересечения', 'FontSize', 14, 'FontName', 'TimesNewRoman');
+xlim([0.056 0.058]);
